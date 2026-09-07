a183169913e20e265a34c88aa6f2e72ab2cb5b9c
525000590 <525000590@lachong.edu.vn>
2026-09-07 21:08:54 +0700

    Fix Ignore TS erors on build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  typescript: {
+    // Bỏ qua kiểm tra lỗi TypeScript khi build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Bỏ qua kiểm tra lỗi ESLint khi build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
